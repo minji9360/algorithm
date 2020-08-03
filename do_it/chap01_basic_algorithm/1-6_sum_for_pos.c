cf83f709490333c1fd7bb03babce1528c91bbcf9
jtbc <jtbc@naver.com>
2020-08-03 13:26:04 +0900

    Add chapter 01-1-6 sum_for_pos.c

---
+++ do_it/chap01_basic_algorithm/1-6_sum_for_pos.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int		main(void)
+{
+	int i;
+	int n;
+	int sum;
+	puts("1부터 n까지의 합을 구합니다.");
+	do
+	{
+		printf("n의 값 : ");
+		scanf("%d", &n);
+	} while (n <= 0);
+	sum = 0;
+	for(i = 1; i <= n; i++)
+		sum += i;
+	printf("1부터 %d까지의 합은 %d입니다.\n", n, sum);
+	return (0);
+}
