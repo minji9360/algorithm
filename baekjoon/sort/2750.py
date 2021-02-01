27f7653b98abee1bfb632dea5a0de3ccfb45e7b1
jtbc <jtbc@naver.com>
2021-02-01 23:12:43 +0900

    Add baekjoon number 2750 about sort

---
+++ baekjoon/sort/2750.py
@@ -0,0 +1,9 @@
+import sys
+
+N = int(sys.stdin.readline())
+numbers = []
+for _ in range(N):
+    numbers.append(int(sys.stdin.readline()))
+numbers.sort(reverse=True)
+for _ in range(N):
+    print(numbers.pop())
