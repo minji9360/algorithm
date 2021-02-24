e071c8c3d8ff8e6b2ce065ef1ab68eea814883fd
jtbc <jtbc@naver.com>
2021-02-24 22:50:35 +0900

    Modify baekjoon number 1439 about greedy

--- baekjoon/greedy/1439.py
+++ baekjoon/greedy/1439.py
@@ -4,7 +4,4 @@ numZero = S.split("1")
 cntOne = len(numOne) - numOne.count("")
 cntZero = len(numZero) - numZero.count("")
 
-if cntOne > cntZero:
-    print(cntZero)
-else:
-    print(cntOne)
+print(min(cntOne, cntZero))
