19a6ea75069e7b998cca64e44daf72ecad0ce65d
Minji Kim <minji4853@naver.com>
2021-05-10 23:43:17 +0900

    feat: Update code using sum method
    If I wanna solve this problem, I need a change of thought.
    first, I use sum method then I can multiply.

--- baekjoon/implement/1225.py
+++ baekjoon/implement/1225.py
@@ -1,11 +1,3 @@
-from itertools import product
-
 num1, num2 = input().split()
-num = [list(map(int, num1)), list(map(int, num2))]
-
-num = list(product(*num))
-result = 0
-
-for x in num:
-    result += x[0] * x[1]
-print(result)
+num1, num2 = list(map(int, num1)), list(map(int, num2))
+print(sum(num1) * sum(num2))
